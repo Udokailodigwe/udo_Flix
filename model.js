dee75a332497cf9757c81d1efaea7dbf305becd2
Udokailodigwe <ilodigweudoka@gmail.com>
2021-11-23 02:30:17 +0200

    Required mongoose to model MongoDB database code

--- model.js
+++ model.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+let moviesSchema = mongoose.Schema({
+   title :{type: String, required: true},
+   description:{type: String, required:true},
+   genre:{
+      name: String,
+      description: String,
+   },
+   director: {
+      name: String,
+      bio: String,
+   },
+   actors:[String],
+   imagepath: String,
+   releasedyear: Number
+});
+
+let usersSchema = mongoose.Schema({
+   username: {type: String, required: true},
+   password: {type: String, required: true},
+   email: {type: String, required: true},
+   birthday: Date,
+   favoriteMovies: [{type: moongose.Schema.Types.ObjectId, ref: 'movies'}]
+});
+
+let movies = mongoose.model('movies', moviesSchema);
+let users = mongoose.model('users', usersSchema);
+
+module.exports.movies = movies;
+module.exports.users  = users;
