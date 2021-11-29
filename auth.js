75df28d6c7f2c02a4daf40f85ba080be66c8b2ee
Udokailodigwe <ilodigweudoka@gmail.com>
2021-11-29 18:26:53 +0200

    Created auth.js file to authenticate http request

---
+++ auth.js
@@ -0,0 +1,34 @@
+const jwtSecret = 'your_jwt_secret'; //similar key used in the JWTStrategy
+const jwt = require ('jsonwebtoken'),
+   passport = require ('passport');
+
+   require ('./passport'); //my local passport file
+
+   let generateJWTToken = (user) => {
+      return jwt.sign(user, jwtSecret, {
+         subject: user.username, //Encoding username in JWT
+         expiresIn: '7d', // Specifying the number of days the token will expire
+         algorithm: 'HS256' // Algorithm used to 'sign' or encode the values of the JWT
+      });
+   }
+
+   /* POST login. */
+   module.exports = (router) => {
+      router.post('/login', (req, res) => {
+         passport.authenticate('local', {session: false}, (error, user, info) => {
+            if (error || !user){
+               return res.status(400).json({
+                  message: 'Something is not right',
+                  user: user
+               });
+            }
+            req.login(user, {session: false}, (error) => {
+               if (error){
+                  res.send(error);
+               }
+               let token = generateJWTToken(user.toJSON());
+               return res.json({user, token}); //returns the token
+            });
+         })(req, res);
+      });
+   }
