9fd9704f160e7efb2fc4f1e334504eaaa6470eca
Udokailodigwe <ilodigweudoka@gmail.com>
2021-12-14 18:17:40 +0200

    Temporarily allowed authorization on movie acess

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/documentation', (req, res) => {
 });
 
 //Get all movies request/route
-app.get('/movies', passport.authenticate ('jwt', {session: false}), (req, res) =>{
+app.get('/movies',  (req, res) =>{
   movies.find()
     .then((movies) => {
       res.status(201).json(movies); 
